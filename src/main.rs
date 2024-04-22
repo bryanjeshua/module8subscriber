ba82ee5cce9ce1ac34e5349afaff6abcec7bc08e
bryanjeshua <bryanjmt04@gmail.com>
2024-04-23 00:22:44 +0700

    Simulation slow subscriber

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
 
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
         
         println!("In Bryan’s Computer [2206027021]. Message received: {:?}", message);
         Ok(())
